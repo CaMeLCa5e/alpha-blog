66a8ebb000f3f812b622c670949f96b42d1d0f73
Jack <jack.martin@avalution.com>
2018-09-05 19:56:42 -0400

    Create template, action, route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,1 +1,17 @@
-articles_controller.rb
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+end
