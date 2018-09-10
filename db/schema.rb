71f16c29451ebf80bce7c86a9386e3e8468bafce
Jack Martin <8301746+CaMeLCa5e@users.noreply.github.com>
2018-09-09 23:11:44 -0400

    Style and homepage updates

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2018_09_05_223932) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
