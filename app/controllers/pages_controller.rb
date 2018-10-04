857842b4a7c80945f8839200754871bc808f83fb
Jack <jack.martin@avalution.com>
2018-10-04 05:14:20 -0400

    updating for authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ActionController::Base
 
-def pages
-end
+  def home
+    redirect_to articles_path if logged_in?
+  end
 
-def about
-end
+  def pages
+  end
+
+  # def about
+  # end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show] 
+  def index
+    @users = User.paginate(page: params[:page], per_page: 7)
+  end
+
   def new
     @user = User.new
   end
@@ -14,12 +19,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
-
   end
 
   def update 
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -28,10 +30,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 7)
+  end
+
+
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
